d7f804886369bf2575e861acb6d60c1ceb56a1e6
Olivier Hoareau <oha@greenberets.io>
2023-06-14 10:48:59 +0200

    feat(findall): add mapper feature

--- packages/findall/src/utils/findAllAndIndexBy.ts
+++ packages/findall/src/utils/findAllAndIndexBy.ts
@@ -1,11 +1,11 @@
-export async function findAllAndIndexBy<T = any>(field: string, fetcher: Function) {
+export async function findAllAndIndexBy<T = any, U = T>(field: string, fetcher: Function, mapper?: (x: T, index: number) => U) {
     let cursor: any = undefined;
     const items: Record<string, T> = {};
     do {
         const page = await fetcher(cursor);
         cursor = page.cursor;
-        page.items?.reduce((acc, item) => {
-            acc[item[field] || ''] = item;
+        page.items?.reduce((acc, item, index: number) => {
+            acc[item[field] || ''] = mapper ? mapper(item, index) : item;
             return acc;
         }, items);
     } while (!!cursor);
