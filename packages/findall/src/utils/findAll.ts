6f2fbc7714f7f65fafac6ecb36257ef0c48bdf4b
Olivier Hoareau <oha@greenberets.io>
2023-06-14 10:19:50 +0200

    feat(findall): add ability to map items

--- packages/findall/src/utils/findAll.ts
+++ packages/findall/src/utils/findAll.ts
@@ -1,11 +1,14 @@
-export async function findAll<T = any>(fetcher: Function) {
+export async function findAll<T = any, U = T>(fetcher: Function, mapper?: (x: T) => U, maxPages: number = 100000) {
     let cursor: any = undefined;
-    let items: T[] = [];
+    let items: U[] = [];
+    let n = 0;
     do {
         const page = await fetcher(cursor);
         cursor = page.cursor;
-        items = [...items, ...page.items];
-    } while (!!cursor);
+        items = [...items, ...(mapper ? (page?.items || []).map(mapper! as any) : (page?.items || []))];
+        n++;
+    } while (!!cursor && (n < maxPages))
+
     return items;
 }
 
