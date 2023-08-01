a8e647a38a82993b2629b6852b8e16a412b8cc16
Olivier Hoareau <oha@greenberets.io>
2023-08-01 17:10:14 +0200

    feat(findall): page size + ctx

--- packages/findall/src/utils/findAll.ts
+++ packages/findall/src/utils/findAll.ts
@@ -1,9 +1,9 @@
-export async function findAll<T = any, U = T>(fetcher: Function, mapper?: (x: T) => U, maxPages: number = 100000) {
+export async function findAll<T = any, U = T>(fetcher: Function, mapper?: (x: T) => U, maxPages: number = 100000, pageSize: number = 200, ctx: Record<string, any> = {}) {
     let cursor: any = undefined;
     let items: U[] = [];
     let n = 0;
     do {
-        const page = await fetcher(cursor);
+        const page = await fetcher(cursor, pageSize, ctx);
         cursor = page.cursor;
         items = [...items, ...(mapper ? (page?.items || []).map(mapper! as any) : (page?.items || []))];
         n++;
