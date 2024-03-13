f26b846c536dffcfcea77ca92ac195d1cb23ea17
carl-mrchnd <66088573+carl-mrchnd@users.noreply.github.com>
2024-03-13 16:35:59 +0100

    feat(dynpackage): add dynpackage (#1)

--- packages/dynpackage/__tests__/index.spec.ts
+++ packages/dynpackage/__tests__/index.spec.ts
@@ -8,4 +8,14 @@ describe('dynpackage', function () {
         expect(zip.getEntryCount()).toEqual(0);
         expect(b).toBeDefined();
     });
+
+    it('is a buffer that contain 2 entries by default', async () => {
+        const b = await dynpackage([
+            ['first', async () => Buffer.from('first test file', 'utf-8')],
+            ['second', async () => Buffer.from('second test file', 'utf-8')],
+        ]);
+        const zip = new AdmZip(b);
+        expect(zip.getEntryCount()).toEqual(2);
+        expect(b).toBeDefined();
+    });
 });
--- packages/dynpackage/src/utils/dynpackage.ts
+++ packages/dynpackage/src/utils/dynpackage.ts
@@ -1,11 +1,14 @@
 import AdmZip from 'adm-zip';
 import { dynpackage_entries } from '../types';
+import { chunkize } from '@ohoareau/array';
 
-export async function dynpackage(entries: dynpackage_entries,{ output }: { output?: string } = {}) {
+export async function dynpackage(entries: dynpackage_entries, { output }: { output?: string } = {}) {
     const zip = new AdmZip();
 
-    // @todo
-    //zip.addFile('test.txt', Buffer.from('the content here', 'utf8'));
+    await chunkize(entries, 20).reduce(async (acc, chunk) => {
+        await acc;
+        return await Promise.allSettled(chunk.map(async (item) => zip.addFile(item[0], await item[1]())));
+    }, Promise.resolve([]) as Promise<any[]>);
 
     const buffer = zip.toBuffer();
 
